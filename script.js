64238d929bd0d12a9d57d7254b77325f00c92a02
Ya2ola <ya2ola@Ya2olas-MacBook-Pro.local>
2026-05-31 15:18:55 +0200

    deleteing chat

--- script.js
+++ script.js
@@ -1087,6 +1087,8 @@
         if (/skriv adress|ange adress|sök adress|enter address|search address/i.test(label)) return 'openBroadbandAddress';
         if (/öppna täckningskarta|coverage map/i.test(label)) return 'openCoverageMap';
         if (/öppna 5g|5g-bredband|broadband/i.test(label)) return 'openBroadbandPage';
+        if (/mina sidor|konto|account/i.test(label)) return 'openAccount';
+        if (/kontakt|support|kundservice|contact/i.test(label)) return 'openContact';
         return null;
       };
 
@@ -1227,6 +1229,16 @@
             } else {
               window.location.href = 'varukorg.html';
             }
+            return;
+          }
+
+          if (suggestion.action === 'openAccount') {
+            window.location.href = 'account.html';
+            return;
+          }
+
+          if (suggestion.action === 'openContact') {
+            window.location.href = 'kontakt.html';
             return;
           }
 
